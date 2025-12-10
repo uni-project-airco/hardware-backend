ee9626855b336df995049d212bee45a58af3d487
dmytro <dziumanenkod@gmail.com>
2025-12-10 14:00:40 +0000

    feat: add logic for pubnub auth token update

--- vendors/pubnub_client.py
+++ vendors/pubnub_client.py
@@ -1,24 +1,25 @@
+import logging
+from typing import Callable, Optional
+
+import requests
+from pubnub.callbacks import SubscribeCallback
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
-from pubnub.callbacks import SubscribeCallback
-from pubnub.enums import PNStatusCategory
-from typing import Callable, Optional
-import json
+
 
 class ThresholdUpdateCallback(SubscribeCallback):
     def __init__(self, message_handler: Optional[Callable] = None):
         self.message_handler = message_handler
 
     def message(self, pubnub, message):
-
         if self.message_handler:
             self.message_handler(message.message)
 
 
-
 class PubNubClient:
 
-    def __init__(self, sub_key: str, pub_key: str, sensor_id: str, chanel_name: str, access_token: str):
+    def __init__(self, sub_key: str, pub_key: str, sensor_id: str, chanel_name: str, access_token: str, server_url: str,
+                 certification_string: str):
         pn_config = PNConfiguration()
         pn_config.subscribe_key = sub_key
         pn_config.publish_key = pub_key
@@ -33,6 +34,8 @@ class PubNubClient:
         self.__sensor_id = sensor_id
         self.__channel = chanel_name
         self.__callback = None
+        self._server_url = server_url
+        self.__certification_string = certification_string
 
     def subscribe(self, message_handler: Optional[Callable] = None):
         self.__callback = ThresholdUpdateCallback(message_handler)
@@ -43,6 +46,21 @@ class PubNubClient:
         if self.__callback:
             self.pubnub.remove_listener(self.__callback)
         self.pubnub.unsubscribe().channels(self.__channel).execute()
+
+    def _send_message(self, message, mtype, ):
+        pb_resource = self.pubnub.publish().channel(self.__channel).message(message).custom_message_type(mtype).sync()
+
+        if pb_resource.status.status_code == 403:
+            logging.info("Updating pubnub auth key")
+            resource = requests.post(url=f'{self._server_url}/sensor/refresh-token',
+                                     headers={
+                                         "certificate-string": self.__certification_string,
+                                         "sensor-id": self.__sensor_id})
+            if resource.ok:
+                self.pubnub.set_token(resource.json()["token"])
+                self.pubnub.publish().channel(self.__channel).message(message).custom_message_type(mtype).sync()
+            else:
+                logging.error(f"Failed to update pubnub auth key: {resource.status_code} - {resource.json()}")
 
     def send_telemetry(self, **kwargs):
         messages = {
@@ -57,7 +75,7 @@ class PubNubClient:
             }
 
         }
-        self.pubnub.publish().channel(self.__channel).message(messages).custom_message_type("sensor-telemetry").sync()
+        self._send_message(message=messages, mtype="sensor-telemetry")
 
     def send_alert(self, title: str, message: str, value, status: str):
         messages = {
@@ -67,4 +85,4 @@ class PubNubClient:
             "value": value,
             "status": status
         }
-        self.pubnub.publish().channel(self.__channel).message(messages).custom_message_type("sensor-alert").sync()
+        self._send_message(message=messages, mtype="sensor-alert")
